30573bdb4bf28bb25cbf36fd60596c012d4ad5d8
GRLG <felicidadkg@gmail.com>
2026-04-04 22:37:51 -0500

    ajustar rutas

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -4,16 +4,21 @@ import { AuthService } from '../services/auth.service';
 
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const auth = inject(AuthService);
-  const token = auth.getToken();
+  //const auth = inject(AuthService);
+  //const token = auth.getToken();
+  const token = localStorage.getItem('token');  // Si hay token y la URL es de nuestro backend en Render, lo añadimos
+  
+  if (req.url.includes('/api/auth/')) {
+    return next(req);
+  }
 
-  // Si hay token y la URL es de nuestro backend en Render, lo añadimos
   if (token && req.url.includes('gym-backend-xwat.onrender.com')) {
-    return next(req.clone({
+    const cloned = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
-    }));
+    });
+  return next(cloned);
   }
 
   // Si no se cumple lo anterior, enviamos la petición tal cual
